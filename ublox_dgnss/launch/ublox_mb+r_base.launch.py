c64ab13302fe8d4532946da2a71fc35156ea3045
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2025-02-12 10:01:29 +1000

    Rebase from 'upstream'

--- ublox_dgnss/launch/ublox_mb+r_base.launch.py
+++ ublox_dgnss/launch/ublox_mb+r_base.launch.py
@@ -2,11 +2,26 @@
 import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
   """Generate launch description for ublox_dgnss components."""
+
+  device_serial_string = LaunchConfiguration('device_serial_string')
+
+  log_level_arg = DeclareLaunchArgument(
+    "log_level", default_value=TextSubstitution(text="INFO")
+  )
+  device_serial_string_arg = DeclareLaunchArgument(
+    "device_serial_string",
+    default_value="Test Base",
+    description="Serial string of the device to use"
+  )
+
   params_base= [
-            {'DEVICE_SERIAL_STRING': "Test Base"},
+            {'DEVICE_SERIAL_STRING': device_serial_string},
             {'FRAME_ID': "base"},
 
             # config measurement interval to 200 ms (ie 5 Hz) and nav update rate to once per measurement
@@ -22,7 +37,7 @@ def generate_launch_description():
             {'CFG_UART1OUTPROT_UBX': False},
 
             # set UART2 baud rate to 460800
-            {'CFG-UART2-BAUDRATE': 0x70800},
+            {'CFG_UART2_BAUDRATE': 0x70800},
 
             # send RTCM messages only (to rover) on UART2
             {'CFG_UART2INPROT_NMEA': False},
@@ -41,17 +56,18 @@ def generate_launch_description():
             {'CFG_USBOUTPROT_UBX': True},
 
             # output RTCM messages required for moving base+rover mode on UART2
-            {'CFG-MSGOUT-RTCM_3X_TYPE4072_0_UART2': 0x1},
-            {'CFG-MSGOUT-RTCM_3X_TYPE1074_UART2': 0x1},
-            {'CFG-MSGOUT-RTCM_3X_TYPE1084_UART2': 0x1},
-            {'CFG-MSGOUT-RTCM_3X_TYPE1124_UART2': 0x1},
-            {'CFG-MSGOUT-RTCM_3X_TYPE1230_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE4072_0_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE1074_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE1084_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE1094_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE1124_UART2': 0x1},
+            {'CFG_MSGOUT_RTCM_3X_TYPE1230_UART2': 0x1},
 
             # messages required for navsatfix calcs by ROS node
             {'CFG_MSGOUT_UBX_NAV_HPPOSLLH_USB': 0x1},
             {'CFG_MSGOUT_UBX_NAV_COV_USB': 0x1},
             {'CFG_MSGOUT_UBX_NAV_STATUS_USB': 0x1},
-            {'CFG_MSGOUT_UBX_NAV_PVT_USB': 0x1},            
+            {'CFG_MSGOUT_UBX_NAV_PVT_USB': 0x1},
             ]
 
   container_base = ComposableNodeContainer(
@@ -59,6 +75,7 @@ def generate_launch_description():
     namespace='',
     package='rclcpp_components',
     executable='component_container_mt',
+    arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
     composable_node_descriptions=[
       ComposableNode(
         package='ublox_dgnss_node',
@@ -75,6 +92,7 @@ def generate_launch_description():
     namespace='',
     package='rclcpp_components',
     executable='component_container_mt',
+    arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
     composable_node_descriptions=[
       ComposableNode(
         package='ublox_nav_sat_fix_hp_node',
@@ -86,6 +104,8 @@ def generate_launch_description():
   )
 
   return launch.LaunchDescription([
+    log_level_arg,
+    device_serial_string_arg,
     container_base,
     container_navsatfix,
     ])
