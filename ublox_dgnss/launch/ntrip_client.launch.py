9c729a4ae582783cc1d9404b98cabc408e1bc48d
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2025-05-29 09:21:35 +1000

    Rebase from 'upstream'

--- ublox_dgnss/launch/ntrip_client.launch.py
+++ ublox_dgnss/launch/ntrip_client.launch.py
@@ -1,4 +1,4 @@
-""" Launch ublox_dgnss_node publishing high precision Lon/Lat messages"""
+""" Launch ntrip client publishing RTCM messages"""
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -28,13 +28,22 @@ def generate_launch_description():
   password_arg = DeclareLaunchArgument(
     "password", default_value=EnvironmentVariable(name="NTRIP_PASSWORD", default_value="password")
   )
+  log_level_arg = DeclareLaunchArgument(
+    "log_level", default_value=TextSubstitution(text="INFO")
+  )
+  maxage_conn_arg = DeclareLaunchArgument(
+    "maxage_conn", default_value=TextSubstitution(text="30")
+  )
+
   params = [{
     'use_https': LaunchConfiguration('use_https'),
     'host': LaunchConfiguration('host'),
     'port': LaunchConfiguration('port'),
     'mountpoint': LaunchConfiguration('mountpoint'),
     'username': LaunchConfiguration('username'),
-    'password': LaunchConfiguration('password')
+    'password': LaunchConfiguration('password'),
+    'log_level': LaunchConfiguration('log_level'),
+    'maxage_conn': LaunchConfiguration('maxage_conn'),
   }]
 
   container1 = ComposableNodeContainer(
@@ -42,12 +51,13 @@ def generate_launch_description():
     namespace='',
     package='rclcpp_components',
     executable='component_container_mt',
+    arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')],
     composable_node_descriptions=[
       ComposableNode(
         package='ntrip_client_node',
         plugin='ublox_dgnss::NTRIPClientNode',
         name='ntrip_client',
-        parameters=params
+        parameters=params,
       )
     ]
   )
@@ -59,5 +69,7 @@ def generate_launch_description():
     mountpoint_arg,
     username_arg,
     password_arg,
+    log_level_arg,
+    maxage_conn_arg,
     container1
   ])
